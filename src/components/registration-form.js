00742825ee79c559b00c2326c785fb08aba9b544
James Stuckey <jstuckey@rutgers.edu>
2018-08-06 16:34:05 -0400

    min password length is 6

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -4,7 +4,7 @@ import { registerUser } from '../actions/users';
 import { login } from '../actions/auth';
 import Input from './input';
 import { required, nonEmpty, matches, length, isTrimmed } from '../validators';
-const passwordLength = length({ min: 10, max: 72 });
+const passwordLength = length({ min: 6, max: 72 });
 const matchesPassword = matches('password');
 
 export class RegistrationForm extends React.Component {
