a380aa20660bae1c7f5e83d0165e816016f13604
Courtney Adams <cadams8238@gmail.com>
2018-08-07 12:07:40 -0700

    added fetch question actions

---
+++ src/actions/questions.js
@@ -0,0 +1,40 @@
+import { API_BASE_URL } from '../config';
+import { normalizeResponseErrors } from './utils';
+import { SubmissionError } from 'redux-form';
+
+
+export const FETCH_QUESTION_REQUEST = 'FETCH_QUESTION_REQUEST';
+export const fetchQuestionRequest = () => ({
+  type: FETCH_QUESTION_REQUEST
+})
+
+export const FETCH_QUESTION_SUCCESS = 'FETCH_QUESTION_SUCCESS';
+export const fetchQuestionSuccess = question => ({
+  type: FETCH_QUESTION_SUCCESS,
+  question
+})
+
+export const FETCH_QUESTION_ERROR = 'FETCH_QUESTION_ERROR';
+export const fetchQuestionError = error => ({
+  type: FETCH_QUESTION_ERROR,
+  error
+})
+
+
+export const getQuestionData = () => (dispatch, getState) => {
+  const authToken = getState().auth.authToken;
+
+  dispatch(fetchQuestionRequest());
+  return (
+    fetch(`${API_BASE_URL}/questions`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    })
+    .then(res => normalizeResponseErrors(res))
+    .then(res => res.json())
+    .then(data => dispatch(fetchQuestionSuccess(data)))
+    .catch(err => dispatch(fetchQuestionError(err)))
+  );
+}
