b493ae55b964ec590b40f4eaa97502054eb90065
James Stuckey <jstuckey@rutgers.edu>
2018-08-08 17:03:59 -0400

    added onSubmit function that grabs the value of user input

--- src/components/emoji-form.js
+++ src/components/emoji-form.js
@@ -4,11 +4,22 @@ import inputStyles from './styles/input.module.css';
 import formStyles from './styles/forms.module.css';
 
 export default class Quiz extends React.Component {
+    onSubmit(event) {
+        event.preventDefault();
+
+        // grabs the value of the input element after you submit the form
+        userAnswer = event.target.answer.value;
+        console.log(userAnswer);
+    }
+
     render() {
         return (
-            <form className={`${inputStyles.formInput} ${formStyles.emojiForm}`}>
+            <form
+                className={`${inputStyles.formInput} ${formStyles.emojiForm}`}
+                onSubmit={event => this.onSubmit(event)}
+            >
                 <label htmlFor="description">Your Answer:</label>
-                <input id="description" type="text" />
+                <input id="description" type="text" name="answer" />
                 <button className={buttonStyles.formButton}>Submit</button>
             </form>
         );
