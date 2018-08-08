42fb755c160afbbb687d33445db9c52a76ac9420
James Stuckey <jstuckey@rutgers.edu>
2018-08-08 16:54:38 -0400

    added async action for next question

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,39 +1,51 @@
 import { API_BASE_URL } from '../config';
 import { normalizeResponseErrors } from './utils';
 
-
 export const FETCH_QUESTION_REQUEST = 'FETCH_QUESTION_REQUEST';
 export const fetchQuestionRequest = () => ({
-  type: FETCH_QUESTION_REQUEST
-})
+    type: FETCH_QUESTION_REQUEST
+});
 
 export const FETCH_QUESTION_SUCCESS = 'FETCH_QUESTION_SUCCESS';
 export const fetchQuestionSuccess = question => ({
-  type: FETCH_QUESTION_SUCCESS,
-  question
-})
+    type: FETCH_QUESTION_SUCCESS,
+    question
+});
 
 export const FETCH_QUESTION_ERROR = 'FETCH_QUESTION_ERROR';
 export const fetchQuestionError = error => ({
-  type: FETCH_QUESTION_ERROR,
-  error
-})
-
+    type: FETCH_QUESTION_ERROR,
+    error
+});
 
 export const getQuestionData = () => (dispatch, getState) => {
-  const authToken = getState().auth.authToken;
+    const authToken = getState().auth.authToken;
 
-  dispatch(fetchQuestionRequest());
-  return (
-    fetch(`${API_BASE_URL}/questions`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${authToken}`
-      }
+    dispatch(fetchQuestionRequest());
+    return fetch(`${API_BASE_URL}/questions`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${authToken}`
+        }
     })
-    .then(res => normalizeResponseErrors(res))
-    .then(res => res.json())
-    .then(data => dispatch(fetchQuestionSuccess(data)))
-    .catch(err => dispatch(fetchQuestionError(err)))
-  );
-}
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(data => dispatch(fetchQuestionSuccess(data)))
+        .catch(err => dispatch(fetchQuestionError(err)));
+};
+
+export const getNextQuestion = () => (dispatch, getState) => {
+    const authToken = getState().auth.authToken;
+
+    dispatch(fetchQuestionRequest());
+    return fetch(`${API_BASE_URL}/questions`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${authToken}`
+        }
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .then(data => dispatch(fetchQuestionSuccess(data)))
+        .catch(err => dispatch(fetchQuestionError(err)));
+};
--- src/components/emoji-form.js
+++ src/components/emoji-form.js
@@ -3,20 +3,14 @@ import buttonStyles from './styles/button.module.css';
 import inputStyles from './styles/input.module.css';
 import formStyles from './styles/forms.module.css';
 
-
-export default function Quiz(props) {
-
-  return (
-    <form className={`${inputStyles.formInput} ${formStyles.emojiForm}`}>
-      <label htmlFor="description">
-        Your Answer:
-      </label>
-      <input id="description" type="text"></input>
-      <button
-        className={buttonStyles.formButton}
-      >
-        Submit
-      </button>
-    </form>
-  );
+export default class Quiz extends React.Component {
+    render() {
+        return (
+            <form className={`${inputStyles.formInput} ${formStyles.emojiForm}`}>
+                <label htmlFor="description">Your Answer:</label>
+                <input id="description" type="text" />
+                <button className={buttonStyles.formButton}>Submit</button>
+            </form>
+        );
+    }
 }
