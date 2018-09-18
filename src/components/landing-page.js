9ecf0c6d02f6d0c3959135839abb5ee2b88682bf
Courtney Adams <cadams8238@gmail.com>
2018-09-18 11:36:16 -0700

    added clouds on desktop version of landing page. working on feature pages. mobile view has basic styling. added all image assets to images folder.

--- src/components/landing-page.js
+++ src/components/landing-page.js
@@ -10,27 +10,67 @@ import styles from './styles/landing-page.module.css';
 export default function LandingPage(props) {
 
     return (
-      <div>
-        <section className={styles.allContent}>
-          <img className={styles.logo}
-            src={emojiLogo}
-            alt="winking emoji"
+      <div className={styles.allContent}>
+        <section className={styles.landing}>
+          <img className={styles.leftCloud}
+            src={require('../images/leftCloud.svg')}
+            alt="cloud illustration"
           />
-          <h1 className={styles.header}>
-            Learn Emoji in just a few minutes a day
-          </h1>
-
-          <Link to="/login"
-            className={styles.buttonLink}
-          >
-            <Button
-              label="Start Learning"
-              className="startButton"
+          <div className={styles.landingContent}>
+            <img className={styles.logo}
+              src={emojiLogo}
+              alt="winking emoji"
             />
-          </Link>
+            <h1 className={styles.header}>
+              Learn Emoji in just a few minutes a day
+            </h1>
+
+            <Link to="/login"
+              className={styles.buttonLink}
+            >
+              <Button
+                label="Start Learning"
+                className="startButton"
+              />
+            </Link>
+          </div>
+          <img className={styles.rightCloud}
+            src={require('../images/rightCloud.svg')}
+            alt="cloud illustration"
+          />
         </section>
 
-        <footer>
+        <section className={styles.featurePage}>
+          <div className={styles.leftCloudBackground}>
+            <div className={styles.mobileImgBkgrd}>
+              <img className={styles.feedback}
+                src={require('../images/feedback.svg')}
+                alt="emoji card feedback"
+              />
+            </div>
+            <h2 className={styles.h2}>
+              Get tested on your emoji vocabulary & get feedback
+              if you're correct or not.
+            </h2>
+          </div>
+        </section>
+
+        <section className={styles.featurePage}>
+          <div className={styles.rightCloudBackground}>
+            <div className={styles.mobileImgBkgrd}>
+              <img className={styles.algo}
+                src={require('../images/algo.svg')}
+                alt="shows how algorithm works when user gets question correct and incorrect"
+              />
+            </div>
+            <h2 className={styles.h2}>
+              Uses a spaced repetition algorithm that allows you
+              to get tested more on what you don't know.
+            </h2>
+          </div>
+        </section>
+
+        <footer className={styles.footer}>
           <p>Made by James & Courtney</p>
         </footer>
       </div>
